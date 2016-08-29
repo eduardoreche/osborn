5eeec0d2fb84b227b6f3aa2133429816c40b5b17
josivan <josivanps@gmail.com>
2016-08-29 10:13:52 -0300

    Logging db connection.

--- components/common/db.js
+++ components/common/db.js
@@ -3,5 +3,13 @@ var dbURI = 'mongodb://localhost/osborn';
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function() {
-  console.log('Mongoose conectado em ' + dbURI);
+    console.log('Mongoose conectado em ' + dbURI);
+});
+
+mongoose.connection.on('error',function (err) {
+    console.log('Erro de conexão Mongoose: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose desconectado');
 });
