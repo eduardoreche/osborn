a4559614080bdd9d3b976bd91ec2caad6ffa823f
Eduardo Reche <edu.reche@gmail.com>
2016-08-31 11:07:47 -0300

    moved to an API approach

--- app.js
+++ app.js
@@ -5,19 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+require('./api/v1/db.js');
 
-require('./components/common/db.js');
-
-var projectRoutes = require('./components/project/project-routes');
-var Project = require('./components/project/project-model');
+var projectRoutes = require('./api/v1/projects');
 
 var app = express(); 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,9 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/projects', projectRoutes);
+app.use('/api/v1/projects', projectRoutes);
 
 app.locals.moment = require('moment')
 
--- public/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-
-  $('a[data-confirm]').on('click', function(e) {
-    if( !confirm( $(this)[0].dataset['confirm']  ) )
-      e.preventDefault();
-  });
