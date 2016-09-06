8247e3f26a5146e99f6792e9dcfaf2bda8380bea
josivan <josivanps@gmail.com>
2016-09-06 15:31:05 -0300

    Creating allocation domain.

---
+++ api/v1/allocations.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+var Allocation = require('../../models/allocation');
+
+router.get('/', (req, res, next) => {    
+    Allocation.find( (err, projects) => {
+      if (err) {
+        res.send(err);
+      }
+      
+      res.json(projects);    
+    });
+});
+
+/*
+router.get('/:id', function(req, res, next){
+  Project.findById(req.params.id, function(err, project){
+    if(err) res.send(err);
+
+    res.json(project);
+  })
+})
+
+router.post('/', function(req, res, next){ 
+  project = new Project({
+      name: req.body.name, 
+      code: req.body.code, 
+      start_date: req.body.start_date, 
+      end_date: req.body.end_date 
+    });
+  
+  project.save(function(err, data) {
+    if(err) res.send(err);
+
+    res.json({message: 'Project saved successfuly!'});
+  });
+})
+
+router.put('/:id', function(req, res, next) {
+  Project.findById(req.params.id, function(err, project) {
+    if(err) res.send(err);
+
+    project.name = req.body.name || project.name;
+    project.code = req.body.code || project.code;
+    project.start_date = req.body.start_date || project.start_date;
+    project.end_date = req.body.end_date || project.end_date;
+
+    project.save(function(err) {
+      if(err) res.send(err);
+      
+      res.json({message: 'Project updated successfuly!'});
+    });
+  })
+})
+
+router.delete('/:id', function(req, res, next) {
+  Project.findById(req.params.id, function(err, project) {
+    if(err) res.send(err);
+
+    project.remove(function(err, data) {
+
+      if(err) res.send(err);
+      
+      res.json({message: 'Project removed successfuly!'});
+
+    });
+  });
+
+})
+
+*/
+module.exports = router;
