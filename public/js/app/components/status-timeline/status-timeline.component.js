6c1decd138886bcf5c25fa42649f5d6748ca563e
Eduardo Reche <edu.reche@gmail.com>
2017-02-16 12:09:38 -0200

    Status timeline component with badges

--- public/js/app/components/status-timeline/status-timeline.component.js
+++ public/js/app/components/status-timeline/status-timeline.component.js
@@ -1,12 +1,19 @@
 const statusTimeline = {
   bindings: {
-    'current-status': '<'
+    'current': '@'
   }, 
-  template: '<div>{{ $ctrl.test }}</div>',
+  templateUrl: 'js/app/components/status-timeline/status-timeline.template.html',
   controller() {
     this.$onInit = () => {
-      this.test = 'test';
-      console.log('component initiated');
+      this.possibleStatus = [
+        {id: 1, desc: 'Oportunidade', icon: 'bullhorn'}, 
+        {id: 2, desc: 'Pré-venda', icon: 'tasks'},
+        {id: 3, desc: 'Em andamento', icon: 'road'},
+        {id: 4, desc: 'Garantia', icon: 'wrench'},
+        {id: 5, desc: 'Encerrada', icon: 'sunglasses'},
+        {id: 6, desc: 'Cancelada', icon: 'thumbs-down'}
+      ];
+      console.log(`status: ${this.current}`);
     }
   }
 }
