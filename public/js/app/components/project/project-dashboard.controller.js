87800049b96c5b4f6420280c94604bf4666ed065
Joabe Tavares Costa <joabetc@hotmail.com>
2017-03-16 15:43:38 -0300

    Adding panel to project dashboard #14

--- public/js/app/components/project/project-dashboard.controller.js
+++ public/js/app/components/project/project-dashboard.controller.js
@@ -80,8 +80,7 @@
             chartArea: {
               left:50,
               top:50,
-              width:'100%',
-              height:'100%'
+              height: 500
             }
           }
         };
