a2cd8819753ec4f8fdf8b9348a190ee7e806b9dc
Joabe Costa <joabetc@hotmail.com>
2017-08-06 17:11:49 -0300

    Rewriting the service in ES6 notation

--- client/src/app/components/kanban-board/kanban-board.service.js
+++ client/src/app/components/kanban-board/kanban-board.service.js
@@ -1,45 +1,32 @@
-(function () {
-  'use strict';
-
-  angular
-    .module('kanbanBoard')
-    .factory('KanbanBoardService', KanbanBoardService);
-
-  KanbanBoardService.inject = ['$q', 'StatusService'];
-
-  function KanbanBoardService($q, StatusService) {
-
-    const service = {
-      canMoveTask,
-      moveTask
-    };
-
-    return service;
-
-    function canMoveTask(sourceColIdVal, targetColIdVal) {
-      let s = '';
-      return $q((resolve, reject) => {
-        StatusService.query(status => {
-          s = status.filter(s => {
-            return s.desc === sourceColIdVal;
-          })[0];
-          if (s.step < targetColIdVal) {
-            resolve(true);
-          } else {
-            reject(false);
-          }
-        });
-      });
-    }
-
-    function moveTask(task, targetColIdVal) {
-      return $q(resolve => {
-        StatusService.query(status => {
-          status = status.filter(s => s.step.toString() === targetColIdVal)[0];
-          task.status = status.desc;
-          resolve(task);
-        });
-      });
-    }
+export class KanbanBoardService {
+  constructor($q, StatusService) {
+    'ngInject';
+    this.$q = $q;
+    this.statusService = StatusService;
   }
-})();
+  canMoveTask(sourceColIdVal, targetColIdVal) {
+    let s = '';
+    return this.$q((resolve, reject) => {
+      this.statusService.query(status => {
+        s = status.filter(s => {
+          return s.desc === sourceColIdVal;
+        })[0];
+        if (s.step < targetColIdVal) {
+          resolve(true);
+        } else {
+          reject(false);
+        }
+      });
+    });
+  }
+  moveTask(task, targetColIdVal) {
+    return this.$q(resolve => {
+      this.statusService.query(status => {
+        status = status.filter(s => s.step.toString() === targetColIdVal)[0];
+        task.status = status.desc;
+        resolve(task);
+      });
+    });
+  }
+}
+
