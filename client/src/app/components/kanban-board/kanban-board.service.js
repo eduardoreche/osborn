54b11f8137ae251cbdb584e71c32913e96127871
Joabe Costa <joabetc@hotmail.com>
2017-08-06 20:40:35 -0300

    Removing external service injection

--- client/src/app/components/kanban-board/kanban-board.service.js
+++ client/src/app/components/kanban-board/kanban-board.service.js
@@ -1,32 +1,25 @@
 export class KanbanBoardService {
-  constructor($q, StatusService) {
+  constructor($q) {
     'ngInject';
     this.$q = $q;
-    this.statusService = StatusService;
   }
-  canMoveTask(sourceColIdVal, targetColIdVal) {
+  canMoveTask(sourceColIdVal, targetColIdVal, statusList) {
     let s = '';
     return this.$q((resolve, reject) => {
-      this.statusService.query(status => {
-        s = status.filter(s => {
-          return s.desc === sourceColIdVal;
-        })[0];
-        if (s.step < targetColIdVal) {
-          resolve(true);
-        } else {
-          reject(false);
-        }
-      });
+      s = statusList.filter(s => {
+        return s.desc === sourceColIdVal;
+      })[0];
+      if (s.step < targetColIdVal) {
+        resolve(true);
+      } else {
+        reject(false);
+      }
     });
   }
-  moveTask(task, targetColIdVal) {
+  moveTask(task, targetColIdVal, statusList) {
     return this.$q(resolve => {
-      this.statusService.query(status => {
-        status = status.filter(s => s.step.toString() === targetColIdVal)[0];
-        task.status = status.desc;
-        resolve(task);
-      });
+      task.status = statusList.filter(s => s.step.toString() === targetColIdVal)[0].desc;
+      resolve(task);
     });
   }
 }
-
